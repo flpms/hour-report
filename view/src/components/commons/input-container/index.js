f29eaa4b6cb1d348c9367bd4d2ee06f0f2592ffa
Filipe M. Silva <filipe.melo.da.silva@accenture.com>
2018-12-09 14:59:54 -0200

    adjust in input state

--- view/src/components/commons/input-container/index.js
+++ view/src/components/commons/input-container/index.js
@@ -2,6 +2,18 @@ import React, { Component } from 'react';
 
 class InputContainer extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(key, event) {
+    this.setState({ value: event.target.value });
+    this.props.onChange(key, event.target.value);
+  }
+
   render() {
     return (<div className="form-group">
       <label htmlFor={this.props.attrKey}>{this.props.label}</label>
@@ -9,6 +21,8 @@ class InputContainer extends Component {
             type="text"
             id={this.props.attrKey}
             placeholder={this.props.placeholder}
+            onChange={(e) => this.onChange(this.props.attrKey, e)}
+            value={this.state.value}
             />
     </div>)
   }
