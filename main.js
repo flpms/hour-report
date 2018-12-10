d4b3534effdb3fa7113dcaba3cc5508705f96a51
flpms <filipemelodasilva@gmail.com>
2018-12-09 23:20:43 -0200

    configure BrowserWindow to load from url at localhost

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
 'use strict'
 
 const { app, BrowserWindow } = require('electron');
+require('./controller/')();
+
+require('./first-run.js')();
+
+let win;
 
 function createWindow () {
   // Create the browser window.
-  const win = new BrowserWindow({ width: 1024, height: 728 });
   // e carrega index.html do app.
-  win.loadUrl('http://localhost:3000');
+  win = new BrowserWindow({
+    width: 1024,
+    height: 728,
+    center: true
+  });
+
+  win.loadURL('http://localhost:3000');
+  win.webContents.openDevTools();
+
+
+  win.on('close', () => {
+    win = null;
+  });
+
+
 }
 
 app.on('ready', createWindow);
