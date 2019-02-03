fcb274b9e76aa47bdd0b49f331298a7046b9b9ed
flpms <filipemelodasilva@gmail.com>
2019-02-03 17:00:23 -0200

    change to use new componentsfor radio and checkbox instead tags

--- view/src/components/publisher/create/index.js
+++ view/src/components/publisher/create/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
-import InputContainer from './../../commons/input-container/index.js';
+import InputContainer from './../../commons/input-container/';
+import RadioButtonContainer from './../../commons/radio-button-container/';
+import CheckboxContainer from './../../commons/checkbox-container/';
 
 const electron = window.require('electron');
 
@@ -15,7 +17,6 @@ class CreatePublisher extends Component {
   }
 
   handleSubmit(evt) {
-    console.log(this.state);
     this.ipc.send('create-publisher', JSON.stringify(this.state));
     evt.preventDefault();
   }
@@ -30,160 +31,117 @@ class CreatePublisher extends Component {
         <h2>Cadastro de Publicador</h2>
         <form onSubmit={this.handleSubmit}>
           <div className="row">
-            <div className="col">
-              <InputContainer attrKey="publisher-name"
-                                label="Nome"
-                                placeholder="José da Silva"
-                                onChange={this.handleInputChange.bind(this)}
-                                />
-            </div>
+            <InputContainer
+              attrKey="publisher-name"
+              label="Nome"
+              placeholder="José da Silva"
+              onChange={this.handleInputChange.bind(this)}
+            />
 
-            <div className="col">
-              <p>Sexo</p>
-              <div className="form-group">
-                <div className="form-check form-check-inline">
-                  <input className="form-check-input"
-                          id="male-sex"
-                          type="radio"
-                          value="m"
-                          name="sex"
-                          onChange={
-                            e => this.handleInputChange.apply(this, ['publisher-sex', e.target.value])
-                          }/>
-                  <label className="form-check-label" htmlFor="male-sex">Masculino</label>
-                </div>
-
-                <div className="form-check form-check-inline">
-                  <input className="form-check-input"
-                          id="female-sex"
-                          type="radio"
-                          value="f"
-                          name="sex"
-                          onChange={
-                            e => this.handleInputChange.apply(this, ['publisher-sex', e.target.value])
-                          }/>
-                  <label className="form-check-label" htmlFor="female-sex">Feminino</label>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <InputContainer attrKey="publisher-address"
-                          label="Endereço"
-                          placeholder="R. Brasil, 273/254D - Jd. Vila Velha - Barueri - SP"
-                          onChange={this.handleInputChange.bind(this)}
-                          />
-
-          <div className="row">
-            <div className="col">
-              <InputContainer attrKey="publisher-phone"
-                              label="Telefone"
-                              placeholder="(11) 913 324 657"
-                              onChange={this.handleInputChange.bind(this)}
-                              />
-            </div>
-            <div className="col">
-              <InputContainer attrKey="publisher-email"
-                              label="Email"
-                              placeholder="nome@provedor.com"
-                              onChange={this.handleInputChange.bind(this)}
-                              />
-            </div>
+            <RadioButtonContainer
+              title="Sexo"
+              attrKey="publisher-sex"
+              onChange={this.handleInputChange.bind(this)}
+              options={[
+                {
+                  id: 'male-sex',
+                  value: 'm',
+                  label: 'Masculino'
+                },
+                {
+                  id: 'female-sex',
+                  value: 'm',
+                  label: 'Feminino'
+                }
+              ]}
+            />
           </div>
 
           <div className="row">
-            <div className="col">
-              <InputContainer attrKey="publisher-birthdate"
-                              label="Data de Nascimento"
-                              placeholder="06/07/1989"
-                              onChange={this.handleInputChange.bind(this)}
-                              />
-            </div>
-
-            <div className="col">
-              <InputContainer attrKey="publisher-immersion"
-                              label="Data de Imersão"
-                              placeholder="25/05/2002"
-                              onChange={this.handleInputChange.bind(this)}
-                              />
-            </div>
+            <InputContainer
+              attrKey="publisher-address"
+              label="Endereço"
+              placeholder="R. Brasil, 273/254D - Jd. Vila Velha - Barueri - SP"
+              onChange={this.handleInputChange.bind(this)}
+            />
           </div>
 
           <div className="row">
-            <div className="form-group col">
-              <p>Ungido ou das Outras Ovelhas</p>
-              <div className="form-check form-check-inline">
-                <input id="anointedOne"
-                        className="form-check-input"
-                        type="radio"
-                        value="true"
-                        name="os"
-                        onChange={
-                          e => this.handleInputChange.apply(this, ['other-sheep', e.target.value])
-                        }/>
-                <label className="form-check-label" htmlFor="anointedOne">Ungido</label>
-              </div>
+            <InputContainer
+              attrKey="publisher-phone"
+              label="Telefone"
+              placeholder="(11) 913 324 657"
+              onChange={this.handleInputChange.bind(this)}
+            />
 
-              <div className="form-check form-check-inline">
-                <input id="otherSheep"
-                        className="form-check-input"
-                        type="radio"
-                        value="false"
-                        name="os"
-                        onChange={
-                          e => this.handleInputChange.apply(this, ['other-sheep', e.target.value])
-                        }/>
-                <label className="form-check-label" htmlFor="otherSheep">Outras Ovelhas</label>
-              </div>
-            </div>
+            <InputContainer
+              attrKey="publisher-email"
+              label="Email"
+              placeholder="nome@provedor.com"
+              onChange={this.handleInputChange.bind(this)}
+              />
+          </div>
 
-            <div className="form-group col">
-              <p>Privilégio de serviço</p>
-              <div className="form-group form-check-inline">
-                <div className="custom-control custom-checkbox">
-                  <input id="regularPionner"
-                          className="form-check-input"
-                          type="checkbox"
-                          value="regular-pionner"
-                          name="ps"
-                          onChange={
-                            e => this.handleInputChange.apply(this, ['regular-pionner', e.target.checked])
-                          }/>
-                  <label className="form-check-label" htmlFor="regularPionner">Pioneiro Regular</label>
-                </div>
+          <div className="row">
+            <InputContainer
+              attrKey="publisher-birthdate"
+              label="Data de Nascimento"
+              placeholder="06/07/1989"
+              onChange={this.handleInputChange.bind(this)}
+            />
 
-                <div className="custom-control custom-checkbox">
-                  <input id="ministerialServant"
-                          className="form-check-input"
-                          type="checkbox"
-                          value="ministerial-servant"
-                          name="ps"
-                          onChange={
-                            e => this.handleInputChange.apply(this, ['ministerial-servant', e.target.checked])
-                          }/>
-                  <label className="form-check-label" htmlFor="ministerialServant">Servo Ministerial</label>
-                </div>
+            <InputContainer
+              attrKey="publisher-immersion"
+              label="Data de Imersão"
+              placeholder="25/05/2002"
+              onChange={this.handleInputChange.bind(this)}
+            />
+          </div>
 
-                <div className="custom-control custom-checkbox">
-                  <input id="elder"
-                          className="form-check-input"
-                          type="checkbox"
-                          value="elder"
-                          name="ps"
-                          onChange={
-                            e => this.handleInputChange.apply(this, ['elder', e.target.checked])
-                          }/>
-                  <label className="form-check-label" htmlFor="elder">Ancião</label>
-                </div>
-              </div>
-            </div>
+          <div className="row">
+            <RadioButtonContainer
+              title="Ungido ou das Outras Ovelhas"
+              attrKey="other-sheep"
+              onChange={this.handleInputChange.bind(this)}
+              options={[
+                {
+                  id: 'anointedOne',
+                  value: true,
+                  label: 'Ungido'
+                },
+                {
+                  id: 'otherSheep',
+                  value: false,
+                  label: 'Outras Ovelhas'
+                }
+              ]}
+            />
+
+            <CheckboxContainer
+              title="Privilégios de serviço"
+              attrKey="ps"
+              onChange={ this.handleInputChange.bind(this) }
+              checks={[
+                {
+                  id: 'regular-pionner',
+                  label: 'Ungido'
+                },
+                {
+                  id: 'ministerial-servant',
+                  label: 'Servo Ministerial'
+                },
+                {
+                  id: 'elder',
+                  label: 'Ancião'
+                }
+              ]}
+            />
           </div>
 
           <div className="offset-md-9 d-flex flex-row-reverse justify-content-around">
             <button type="submit" className="btn btn-outline-primary">Salvar</button>
             <button type="submit" className="btn btn-outline-secondary">Cadastrar Outro</button>
           </div>
-
         </form>
       </div>);
   }
